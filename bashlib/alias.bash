cb1bf4070f4fd25c58c0fa469a8b4ab26bc412ce
Giacomo Tagliabue <giacomo.tag@gmail.com>
2015-07-02 14:27:36 +0200

    add gdc alias for cached diff

--- bashlib/alias.bash
+++ bashlib/alias.bash
@@ -42,6 +42,7 @@ alias glgs="git log --graph --abbrev-commit --decorate --date=relative --format=
 alias gl='git pull'
 alias gp='git push'
 alias gd='git diff'
+alias gdc='git diff --cached'
 
 #docker aliases
 alias dockerstart="boot2docker start && export DOCKER_HOST=tcp://192.168.59.103:2376 && export DOCKER_CERT_PATH='C:\Users\giaco_000\.boot2docker\certs\boot2docker-vm' && export DOCKER_TLS_VERIFY=1"
